75c35afd2317d768ffeca2bc543fb28864bb6dbc
AlexSaunders <alexjrsaunders@gmail.com>
2017-02-07 00:27:20 +0000

    Grunt config changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,16 +22,31 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 7
+				},
+				files: [{
+					src: ['static/img/**/*.{png,jpg}', 'static/img/*.{png,jpg}']
+				}]
+			}
+		},
 		watch: {
 			css: {
 				files: ['themes/material/static/sass/*.scss', 'themes/material/static/css/style.css'],
 				tasks: ['sass', 'autoprefixer', 'cssmin']
+			},
+			image: {
+				files: ['static/img/*.png', 'static/img/*.jpg', 'static/img/**/*.png', 'static/img/**/*.jpg'],
+				tasks: ['imagemin']
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin']);
+	grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin', 'imagemin']);
 }
